7b44aa620b238da7cc63efc84ee7a1ef09e7ffac
Alice-Retz <alicegretz@gmail.com>
2021-07-20 10:26:56 -0500

    Troubleshooting link issues

--- home/home.js
+++ home/home.js
@@ -6,8 +6,10 @@ const userForm = document.getElementById('user-form');
 userForm.addEventListener('submit', e => {
     e.preventDefault();
     const formData = new FormData(userForm);
+    console.log(formData.get('name'));
     const newUser = createUser(formData);
+    console.log(newUser);
     saveUser(newUser);
-    window.location = '../map/index.html';
+    window.location = 'map';
 });
 
